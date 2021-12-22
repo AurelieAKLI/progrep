b6feb69ea0640c762c6b41dd3294f660d075be92
AurelieAKLI <60830313+AurelieAKLI@users.noreply.github.com>
2021-12-22 20:21:32 +0100

    Update annuaire.c

--- annuaire.c
+++ annuaire.c
@@ -1,74 +1,48 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define N 10
-#define PORT_NUMBER 0
+#define PORT 5555
 
-char *message_in="le sommet vers annuaire\n";
-char *message_out="l'annuaire vers sommet\n";
+void main(){
 
-int main(int argc, char **argv) {
-        int domaine=AF_INET;
-        int type=SOCK_STREAM;
-        int protocole=0;
-        printf("1\n");
+        int sockEcoute;
+        struct sockaddr_in annuaire;
 
-        int sid_ecoute = socket(domaine, type, protocole);
-        if (sid_ecoute == -1) {
-                perror("Socket creation error");
-                return 1;
-        }
-        printf("2\n");
+        int sockAccept;
+        struct sockaddr_in adrSommet;
 
-        struct sockaddr_in serv_sommet;
-        //demande de connexion au serveur
-        memset(&serv_sommet, 0, sizeof(serv_sommet));
-        serv_sommet.sin_family = domaine;
-        serv_sommet.sin_port = htons(PORT_NUMBER);
-        serv_sommet.sin_addr.s_addr = htonl(INADDR_ANY);;
+        socklen_t taille_ad;
+        char msg[1024];
 
-        socklen_t ad=sizeof(serv_sommet);
-        printf("3\n");
+        sockEcoute = socket(AF_INET, SOCK_STREAM, 0);
+        printf("Création socket de l'annuaire réussie.\n");
+        memset(&annuaire, '\0', sizeof(annuaire));
 
-        if (bind(sid_ecoute, (struct sockaddr*) &serv_sommet, ad )==-1) {
-                perror("Bind error");
-                close(sid_ecoute);
-                return 2;
-        }
+        annuaire.sin_family = AF_INET;
+        annuaire.sin_port = htons(PORT);
+        annuaire.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-        //inet_pton(domaine, "127.0.0.1", &(serv_sommet.sin_addr));
-        printf("4\n");
+        bind(sockEcoute, (struct sockaddr*)&annuaire, sizeof(annuaire));
+        printf("Lié au port numero [%d].\n", 4455);
 
-        if (listen(sid_ecoute, 1)==-1){ //jusqu'à 15 clients dans la fille d'attente
-                perror("Listen error");
-                close(sid_ecoute);
-                return 3;
-        }
-        printf("5\n");
+        listen(sockEcoute, 5);
+        printf("En écoute...\n");
 
+        sockAccept = accept(sockEcoute, (struct sockaddr*)&adrSommet, &taille_ad);
 
-        if ( accept(sid_ecoute, (struct sockaddr *) &serv_sommet, &ad)== -1){
-                perror("error accept");
-                return 4;
-        }
+        strcpy(msg, "Bonjour");
 
-        printf("6\n");
+        send(sockAccept, msg, strlen(msg), 0);
+        recv(sockAccept, msg, 1024, 0);
 
-        while (1){
-                printf("7\n");
-                recv(sid_ecoute, &message_in, 4,0);
-                send(sid_ecoute, "c'est l'annuaire", 4, 0);
+        printf("%s\n", msg);
+        printf("Fermeture de la connexion.\n");
 
-        }
-  
-        return 0;
 
 
 }
