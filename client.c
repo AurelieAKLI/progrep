f8c01188ef961bfa77e0e01658ecbe9b56800632
AurelieAKLI <60830313+AurelieAKLI@users.noreply.github.com>
2021-12-28 14:13:54 +0100

    Update client.c

--- client.c
+++ client.c
@@ -10,131 +10,100 @@
 #include <pthread.h>
 
 #define LENGTH 2048
+#define MAX_SOMMETS 50
 
 // Global variables
 volatile sig_atomic_t flag = 0;
-int sockfd = 0;
-char name[32];
+int sock = 0;
+char liste[MAX_SOMMETS][MAX_SOMMETS];
+int compteur=0;
+
 
 void str_overwrite_stdout() {
         printf("%s", "> ");
         fflush(stdout);
 }
 
-void str_trim_lf (char* arr, int length) {
-        int i;
-        for (i = 0; i < length; i++) { // trim \n
-                if (arr[i] == '\n') {
-                        arr[i] = '\0';
-                        break;
-                }
-        }
-}
 
 void catch_ctrl_c_and_exit(int sig) {
         flag = 1;
 }
-
+/*
 void send_msg_handler() {
         char message[LENGTH] = {};
         char buffer[LENGTH + 32] = {};
 
         while(1) {
-                str_overwrite_stdout();
-                fgets(message, LENGTH, stdin);
-                str_trim_lf(message, LENGTH);
 
-                if (strcmp(message, "exit") == 0) {
-                        break;
-                } else {
-                        sprintf(buffer, "%s: %s\n", name, message);
+                        sprintf(buffer, "%s: %d\n", name, gethostname(ad_port,100));
                         send(sockfd, buffer, strlen(buffer), 0);
                 }
 
-                bzero(message, LENGTH);
-                bzero(buffer, LENGTH + 32);
         }
         catch_ctrl_c_and_exit(2);
-}
+}*/
 
 void recv_msg_handler() {
         char message[LENGTH] = {};
         while (1) {
-                int receive = recv(sockfd, message, LENGTH, 0);
-                if (receive > 0) {
-                        printf("%s", message);
-                        str_overwrite_stdout();
-                } else if (receive == 0) {
+                int recu = recv(sock, message, LENGTH, 0);
+                if (recu > 0) {
+                        strcpy(liste[compteur],message);
+                        printf("%s\n",liste[compteur]);
+                        compteur++;
+                        //str_overwrite_stdout();
+                } else if (recu == 0) {
                         break;
                 } else {
-                        // -1
+                        perror("Probleme recv");
                 }
-                memset(message, 0, sizeof(message));
+
         }
 }
 
 int main(int argc, char **argv){
         if(argc != 2){
-                printf("Usage: %s <port>\n", argv[0]);
+                printf("Usage: %s <num_port>\n", argv[0]);
                 return EXIT_FAILURE;
         }
 
-        char *ip = "127.0.0.1";
-        int port = atoi(argv[1]);
+        char *adr_ip = "127.0.0.1";
+        int num_port = atoi(argv[1]);
 
         signal(SIGINT, catch_ctrl_c_and_exit);
 
-        printf("Please enter your name: ");
-        fgets(name, 32, stdin);
-        str_trim_lf(name, strlen(name));
+        struct sockaddr_in adr_annuaire;
+
+        //Paramètres socket
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        adr_annuaire.sin_family = AF_INET;
+        adr_annuaire.sin_addr.s_addr = inet_addr(adr_ip);
+        adr_annuaire.sin_port = htons(num_port);
 
 
-        if (strlen(name) > 32 || strlen(name) < 2){
-                printf("Name must be less than 30 and more than 2 characters.\n");
-                return EXIT_FAILURE;
-        }
-
-        struct sockaddr_in server_addr;
-
-        /* Socket settings */
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_addr.s_addr = inet_addr(ip);
-        server_addr.sin_port = htons(port);
-
-
-        // Connect to Server
-        int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        // Connexion à l'annuaire
+        int err = connect(sock, (struct sockaddr *)&adr_annuaire, sizeof(adr_annuaire));
         if (err == -1) {
-                printf("ERROR: connect\n");
+                perror("Problème connexion");
                 return EXIT_FAILURE;
         }
 
-        // Send name
-        send(sockfd, name, 32, 0);
-
-        printf("=== WELCOME TO THE CHATROOM ===\n");
-
-        pthread_t send_msg_thread;
-        if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0){
-                printf("ERROR: pthread\n");
-                return EXIT_FAILURE;
-        }
 
         pthread_t recv_msg_thread;
         if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0){
-                printf("ERROR: pthread\n");
+                perror("Problème création thread");
                 return EXIT_FAILURE;
         }
 
+
         while (1){
                 if(flag){
-                        printf("\nBye\n");
+                        printf("\nFin\n");
                         break;
                 }
         }
-
-        close(sockfd);
+        pthread_join(recv_msg_thread, NULL);
+        close(sock);
 
         return EXIT_SUCCESS;
 }
